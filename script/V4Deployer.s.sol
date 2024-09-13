f7c39df71e94eb58d9dbc63d25585f259cb43f59
Haardik <haardik@haardik.dev>
2024-09-13 09:17:54 -0400

    sample using predeployed v4 core contracts

--- script/V4Deployer.s.sol
+++ script/V4Deployer.s.sol
@@ -9,18 +9,29 @@ import {PoolDonateTest} from "v4-core/test/PoolDonateTest.sol";
 import {PoolTakeTest} from "v4-core/test/PoolTakeTest.sol";
 import {PoolClaimsTest} from "v4-core/test/PoolClaimsTest.sol";
 
+import "forge-std/console.sol";
+
 contract V4Deployer is Script {
     function run() public {
         vm.startBroadcast();
 
         PoolManager manager = new PoolManager();
+        console.log("Deployed PoolManager at", address(manager));
         PoolSwapTest swapRouter = new PoolSwapTest(manager);
+        console.log("Deployed PoolSwapTest at", address(swapRouter));
         PoolModifyLiquidityTest modifyLiquidityRouter = new PoolModifyLiquidityTest(
                 manager
             );
+        console.log(
+            "Deployed PoolModifyLiquidityTest at",
+            address(modifyLiquidityRouter)
+        );
         PoolDonateTest donateRouter = new PoolDonateTest(manager);
+        console.log("Deployed PoolDonateTest at", address(donateRouter));
         PoolTakeTest takeRouter = new PoolTakeTest(manager);
+        console.log("Deployed PoolTakeTest at", address(takeRouter));
         PoolClaimsTest claimsRouter = new PoolClaimsTest(manager);
+        console.log("Deployed PoolClaimsTest at", address(claimsRouter));
 
         // Anything else you need to do like minting mock ERC20s or initializing a pool
         // you need to do directly here as well without using Deployers
